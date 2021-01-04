4f0bbd9785ebf87370d76ab450d8dc906757f5c3
Idriss Ben Hmida <45881349+idrisso4@users.noreply.github.com>
2021-01-04 21:03:25 +0100

    Add files via upload

---
+++ edit_image.py
@@ -0,0 +1,32 @@
+# Author: idrisso4
+
+# Import Libraries
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+
+def lecture_img(file):
+    img=mpimg.imread(file)
+    return(img)
+
+def r90d(im):
+    hauteur, largeur, couleur = im.shape
+    imgd = np.zeros((largeur,hauteur,couleur))
+    for i in range(hauteur):
+        imgd[:,hauteur-i-1]=im[i]
+    return(imgd)
+
+def green(img):
+    imgvert = np.zeros(img.shape)
+    imgvert [ : , : , 1] = img [ : , : , 1]
+    return(imgvert)
+
+def negatif(img):
+    imginverse = 1-img
+    return(imginverse)
+
+def gray(img):
+    imggris = (img[ : , : , 0] + img[ : , : , 1] + img[ : , : , 2])/3
+    return(imggris)
+
+
